6353690307ad5a44d3791f451c387f78a95a4644
Juan Carlos Ortiz <57382762+JUANORTIZR@users.noreply.github.com>
2020-10-03 20:58:34 -0500

    Probando commit desde terminal

--- app/src/main/java/com/proyectconpany/pulsacioneskotlin/MainActivity.kt
+++ app/src/main/java/com/proyectconpany/pulsacioneskotlin/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    //Probando commit 
+    //Probando commit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
